ac680185c101d54c2cdbb1d8f42c097ece0e09d6
Ingy döt Net <ingy@ingy.net>
2020-07-14 14:54:17 -0700

    wip

--- squareup-plus.user.js
+++ squareup-plus.user.js
@@ -2,7 +2,7 @@
 // @name         Red Background
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Turns website background to red
+// @description  SquareUp improvements for Good Weather
 // @author       Ingy döt Net
 // @require      http://coffeescript.org/browser-compiler/coffeescript.js
 // @match        http://ingy.net
@@ -11,7 +11,7 @@
 var user_script_coffee = (<><![CDATA[
 //#----------------------------------------------------------------------------
 
-document.body.style.background = 'red'
+alert 'OK'
 
 //#----------------------------------------------------------------------------
 ]]></>).toString(); eval(this.CoffeeScript.compile(user_script_coffee));
