448142c9bce2a0555b493fffe5906533f7209278
Ingy döt Net <ingy@ingy.net>
2020-07-14 14:57:38 -0700

    wip

--- squareup-plus.user.js
+++ squareup-plus.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         Red Background
 // @namespace    http://tampermonkey.net/
-// @version      0.1
 // @description  SquareUp improvements for Good Weather
 // @author       Ingy döt Net
 // @require      http://coffeescript.org/browser-compiler/coffeescript.js
-// @match        http://ingy.net
 // @match        https://www.goodweatherinseattle.com/
+// @grant        unsafeWindow
+// @version      0.1
 // ==/UserScript==
 var user_script_coffee = (<><![CDATA[
 //#----------------------------------------------------------------------------
 
-alert 'OK'
+window.w = window.unsafeWindow = unsafeWindow
+
+document.title = 'Hacked 1'
 
 //#----------------------------------------------------------------------------
 ]]></>).toString(); eval(this.CoffeeScript.compile(user_script_coffee));
