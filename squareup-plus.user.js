ff3c5c758fd779e440eb2b3a9d33cdcc34cff747
Ingy döt Net <ingy@ingy.net>
2020-07-14 15:14:48 -0700

    wip

--- squareup-plus.user.js
+++ squareup-plus.user.js
@@ -20,7 +20,6 @@ window.w = window.unsafeWindow = unsafeWindow
 
 document.title = 'Hacked 1'
 
-#---------------------------------------------------------------------
-#---------
+#------------------------------------------------------------------------------
 ]]></>).toString(); eval(this.CoffeeScript.compile(user_script_coffee));
 //# vim: set ft=coffee sw=2:
