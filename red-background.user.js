b53eaad6affdd97daf3a6cc4995126423e421e5f
Ingy döt Net <ingy@ingy.net>
2020-07-14 14:44:55 -0700

    wip

--- red-background.user.js
+++ red-background.user.js
@@ -2,18 +2,18 @@
 // @name         Red Background
 // @namespace    https://github.com/
 // @updateURL    https://github.com/goodweatherinseattle/tampermonkey/raw/master/red-background.user.js
-// @version      0.4
 // @description  Turns website background to red
 // @author       Ingy döt Net
 // @require      http://coffeescript.org/browser-compiler/coffeescript.js
 // @match        http://ingy.net
 // @match        https://www.goodweatherinseattle.com/
+// @version      0.5
 // ==/UserScript==
 
 var user_script_coffee = (<><![CDATA[
 #------------------------------------------------------------------------------
 
-document.body.style.background = 'pink'
+document.body.style.background = 'cyan'
 
 #------------------------------------------------------------------------------
 ]]></>).toString(); eval(this.CoffeeScript.compile(user_script_coffee));
